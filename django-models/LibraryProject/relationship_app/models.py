02424cc6093faaca55be6fad92b8b0f5992eec70
clinton <clintonkipkemoi2@gmail.com>
2025-02-25 22:23:34 +0300

    update model.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,14 +5,26 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
 
 class library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
+    def __str__(self):
+        return self.name
+
 class librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
