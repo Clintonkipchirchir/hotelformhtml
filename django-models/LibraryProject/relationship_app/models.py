eff17bac7d418b293e27fb2aa52809e013f7f62b
clinton <clintonkipkemoi2@gmail.com>
2025-02-27 02:56:19 +0300

    update

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import UserManager
+
+# Create user
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
 
 # Create your models here.
 class Author(models.Model):
@@ -28,3 +45,4 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
