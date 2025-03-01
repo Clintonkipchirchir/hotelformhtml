fdd954dd8df57025cd57aabfb0d88b662d560df2
clinton <clintonkipkemoi2@gmail.com>
2025-03-01 12:16:35 +0300

    updated login logout and registration of user

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.all_books, name='all_books'),
     path('library_details/', views.library_details, name='library_details'),
     path('register/', views.register, name='register'),
-    path('login/', LoginView.as_veiw(template_name="login.html"), name='login'),
+    path('login/', LoginView.as_view(template_name="login.html"), name='login'),
     path('logout/', LogoutView.as_view(template_name="logout.html"), name='logout'),
 ]
 
