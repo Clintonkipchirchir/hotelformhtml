50a70df05dfbe35b4e12de6bd4229f4af2c5359e
clinton <clintonkipkemoi2@gmail.com>
2025-03-02 13:05:55 +0300

    Update to  permission for admin librarian and members

--- django-models/LibraryProject/relationship_app/librarian_view.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render	
-from django.contrib.auth.decorators import login_required, user_passes_test
-
-@login_required
-@user_passes_test(lambda user: user.userprofile.role == 'Librarian')
-def librarian_view(request):
-    return render(request, 'librarian.html')
--- django-models/LibraryProject/relationship_app/member_view.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render	
-from django.contrib.auth.decorators import login_required, user_passes_test
-
-@login_required
-@user_passes_test(lambda user: user.userprofile.role == 'Member')
-def member_view(request):
-    return render(request, 'member.html')
