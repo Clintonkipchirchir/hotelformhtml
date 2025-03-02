7acd9ee7172c2a1a86fbe4cf79f4cffbde0cfd0b
clinton <clintonkipkemoi2@gmail.com>
2025-03-02 13:33:10 +0300

    Update addition of perms required for view books

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.contrib.auth import login , logout
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
