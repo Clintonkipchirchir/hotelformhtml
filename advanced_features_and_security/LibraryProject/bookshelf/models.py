a713d1ef2c0034524c094930508a9a4c633009ed
clinton <clintonkipkemoi2@gmail.com>
2025-03-02 22:01:33 +0300

    added perms for editors, viewers and admins

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -5,20 +5,35 @@ from django.contrib.auth.models import AbstractUser
 
 # Create a custom user manager
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, username, date_of_birth, profile_photo, email, password=None, **extra_fields):
         if not email:
             raise ValueError('The email is required')
+        if not username:
+            raise ValueError('The username is required')
         
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(
+            email=email,
+            username=username,
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo, 
+            **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password=None, **extrafields):
+    def create_superuser(self, username, date_of_birth, profile_photo, email, password=None, **extra_fields):
         extrafields.setdefault('is_staff', True)
         extrafields.setdefault('is_superuser', True)
-        return self.create_user(email, password, **extrafields)
+        user = self.create_user(
+            email=email,
+            username=username,
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo, 
+            **extra_fields)
+
+        user.save()
+        return user
 
 
 #create custom user model
@@ -36,4 +51,19 @@ class Book(models.Model):
   
     def __str__(self):
         return self.title
+
+#create permission for the users
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ('can_view', 'can_view'),
+        ('can_create', 'can_create'),
+        ('can_edit', 'can_edit'),
+        ('can_delete', 'can_delete'),   
+    )
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    role = models.CharField(max_length=100, choices=ROLE_CHOICES, default='can_view')
+    
+    def __str__(self):
+        return self.user.username
+    
 
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 # Create your views here.
+
+@permission_required(('bookshelf.can_edit', 'bookshelf.can_create'), raise_exception=True)
+def Editors(request):
+    return render(request, 'bookshelf/book_list.html')
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def Viewers(request):
+    return render(request, 'bookshelf/book_list.html')
+
+@permission_required(('bookshelf.can_edit', 'bookshelf.can_create', 'bookshelf.can_delete', 'bookshelf.can_view'), raise_exception=True)
+def Admins(request):
+    return render(request, 'bookshelf/book_list.html')
